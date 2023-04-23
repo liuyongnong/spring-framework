b83dee0bde39384cd5f18302de8af629194d2835
liuyn <1965737383@qq.com>
2023-04-23 17:45:19 +0800

    commit

---
+++ spring-liuyn/src/main/java/org/springframework/bean/UserDao.java
@@ -0,0 +1,31 @@
+package org.springframework.bean;
+
+public class UserDao {
+    private String userName;
+    private String work;
+    private int age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
